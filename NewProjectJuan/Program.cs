fa4a5b315065e5526f71249d8f5f127f08e38167
Nuranakhmed <nuranakhmedova@gmail.com>
2023-01-03 00:46:45 +0400

    1

--- NewProjectJuan/Program.cs
+++ NewProjectJuan/Program.cs
@@ -1,9 +1,21 @@
+using DataAccess.Contexts;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 
+var connectionstr = builder.Configuration["ConnectionStrings:Default"];
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+    opt.UseSqlServer(connectionstr);
+});
+
+
+
 var app = builder.Build();
 app.UseStaticFiles();
+
 app.MapControllerRoute(
     name: "juan",
     pattern: "{controller=Home}/{action=Index}/{Id?}");
