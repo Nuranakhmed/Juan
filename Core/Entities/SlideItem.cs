7050b7cf891c2dcbe274e5f4f0020e19a346eb4b
Nuranakhmed <nuranakhmedova@gmail.com>
2023-01-02 22:59:47 +0400

    yarmciq

---
+++ Core/Entities/SlideItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities;
+
+public class SlideItem
+{
+    public int Id { get; set; }
+    public string? subtitle { get; set; }
+    public string? title { get; set; }
+    public string? Description { get; set; }
+
+}
